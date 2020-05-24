0734f4f2e50da37d4683fb1ed37bee5a5d79bfb0
Eric Zepeda <zepedaeric@gmail.com>
2020-05-24 11:15:59 -0700

    install dependencies

--- server/index.js
+++ server/index.js
@@ -7,15 +7,15 @@ var bodyParser = require('body-parser');
 var app = express();
 
 // UNCOMMENT FOR REACT
-// app.use(express.static(__dirname + '/../react-client/dist'));
+app.use(express.static(__dirname + '/../react-client/dist'));
 
 // UNCOMMENT FOR ANGULAR
 // app.use(express.static(__dirname + '/../angular-client'));
 // app.use(express.static(__dirname + '/../node_modules'));
 
 app.get('/items', function (req, res) {
-  items.selectAll(function(err, data) {
-    if(err) {
+  items.selectAll(function (err, data) {
+    if (err) {
       res.sendStatus(500);
     } else {
       res.json(data);
@@ -23,7 +23,7 @@ app.get('/items', function (req, res) {
   });
 });
 
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log('listening on port 3000!');
 });
 
