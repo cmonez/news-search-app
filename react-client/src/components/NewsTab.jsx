bea7141d2616799c790383ae9955174313bd8b01
Eric Zepeda <zepedaeric@gmail.com>
2020-05-28 12:52:37 -0700

    Turn NewsTabs into a class component

--- react-client/src/components/NewsTab.jsx
+++ react-client/src/components/NewsTab.jsx
@@ -2,27 +2,33 @@ import React from 'react';
 import Tabs from 'react-bootstrap/Tabs';
 import Tab from 'react-bootstrap/Tab';
 import Nav from 'react-bootstrap/Nav';
+import ReactDOM from 'react-dom';
 
-const NewsTabs = (props) => {
-
-
-  return (
-    <Tabs defaultActiveKey="Browse" id="uncontrolled-tab-example" onSelect={(eventKey) => {
-      if (eventKey === 'Saved') {
-        console.log('Will retrieve saved articles')
-      } else {
-        console.log('Keep browsing!')
-      }
-    }}>
-      <Tab eventKey="Browse" title="Browse" >
-        {/* <Sonnet /> */}
-      </Tab>
-      <Tab eventKey="Saved" title="Saved">
-        {/* <Sonnet /> */}
-      </Tab>
-
-    </Tabs>
-  );
+class NewsTabs extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      stateful: true
+    }
+  }
+  render() {
+    return (
+      <Tabs defaultActiveKey="Browse" id="uncontrolled-tab-example" onSelect={(eventKey) => {
+        if (eventKey === 'Saved') {
+          console.log('Will retrieve saved articles')
+        } else {
+          console.log('Keep browsing!')
+        }
+      }}>
+        <Tab eventKey="Browse" title="Browse" >
+          {/* <Sonnet /> */}
+        </Tab>
+        <Tab eventKey="Saved" title="Saved">
+          {/* <Sonnet /> */}
+        </Tab>
+      </Tabs>
+    )
+  }
 }
 
 
